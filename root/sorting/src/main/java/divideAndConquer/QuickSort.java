97bf14af424726d40239d7e6a371cb2f93850cd8
abrahammunoz <abrahammunoz@localhost>
2023-10-15 18:30:13 -0400

    Commit #2: Made methods public so they can be decompiled.

--- root/sorting/src/main/java/divideAndConquer/QuickSort.java
+++ root/sorting/src/main/java/divideAndConquer/QuickSort.java
@@ -13,7 +13,7 @@ public class QuickSort {
         quickSort(arr, 0, arr.length - 1);
     }
 
-    private static void quickSort(int[] arr, int low, int high) {
+    public static void quickSort(int[] arr, int low, int high) {
         if (low < high) {
             int pivotIndex = partition(arr, low, high);
             quickSort(arr, low, pivotIndex - 1);
@@ -21,7 +21,7 @@ public class QuickSort {
         }
     }
 
-    private static int partition(int[] arr, int low, int high) {
+    public static int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
         int i = low - 1;
         for (int j = low; j < high; j++) {
@@ -34,7 +34,7 @@ public class QuickSort {
         return i + 1;
     }
 
-    private static void swap(int[] arr, int i, int j) {
+    public  static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
