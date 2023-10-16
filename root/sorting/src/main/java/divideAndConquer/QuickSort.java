a712723434a60951d6d8cc793616e5c726b471fa
munoabr8 <munoabr8@gmail.com>
2023-10-15 20:42:41 -0400

    Commit #3: removed a parameter from the functions

--- root/sorting/src/main/java/divideAndConquer/QuickSort.java
+++ root/sorting/src/main/java/divideAndConquer/QuickSort.java
@@ -3,40 +3,65 @@ package divideAndConquer;
 import java.util.Arrays;
 
 public class QuickSort {
-    public static void main(String[] args) {
-        int[] arr = {5, 2, 9, 3, 6, 4, 8, 1, 7};
-        quickSort(arr);
-        System.out.println("Sorted Array: " + Arrays.toString(arr));
-    }
 
-    public static void quickSort(int[] arr) {
-        quickSort(arr, 0, arr.length - 1);
-    }
+	private static int[] sortMe;
 
-    public static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pivotIndex = partition(arr, low, high);
-            quickSort(arr, low, pivotIndex - 1);
-            quickSort(arr, pivotIndex + 1, high);
-        }
-    }
+	public QuickSort(int[] array) {
 
-    public static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                swap(arr, i, j);
-            }
-        }
-        swap(arr, i + 1, high);
-        return i + 1;
-    }
+		sortMe = array;
 
-    public  static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+	}
+
+	public static void main(String[] args) {
+
+		int[] arr = new int[] { 5, 2, 9, 3, 6, 10,4, 8, 1, 7 };
+
+		QuickSort sortingAlo = new QuickSort(arr);
+		sortingAlo.sort();
+	}
+
+	public void sort() {
+
+		quickSort();
+
+		display();
+
+	}
+
+	void display() {
+
+		System.out.println("Sorted Array: " + Arrays.toString(sortMe));
+
+	}
+
+	public static void quickSort() {
+		quickSort(0, sortMe.length - 1);
+	}
+
+	public static void quickSort(int low, int high) {
+		if (low < high) {
+			int pivotIndex = partition(low, high);
+			quickSort(low, pivotIndex - 1);
+			quickSort(pivotIndex + 1, high);
+		}
+	}
+
+	public static int partition(int low, int high) {
+		int pivot = sortMe[high];
+		int i = low - 1;
+		for (int j = low; j < high; j++) {
+			if (sortMe[j] < pivot) {
+				i++;
+				swap(i, j);
+			}
+		}
+		swap(i + 1, high);
+		return i + 1;
+	}
+
+	public static void swap(int i, int j) {
+		int temp = sortMe[i];
+		sortMe[i] = sortMe[j];
+		sortMe[j] = temp;
+	}
 }
